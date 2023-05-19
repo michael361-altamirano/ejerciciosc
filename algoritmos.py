10917429a41aa83330c43f96ce8ea128b091c35c
michael361-altamirano <michael22067@gmail.com>
2023-05-19 18:52:39 -0500

    Algoritmos realizados en py y en javascript

---
+++ algoritmos.py
@@ -0,0 +1,67 @@
+# 1. haga un algoritmo que calcule los dias trabajados de un empleado,
+# si el empleado elige 1 su salario diario corresponde a 200, si elige 2 su salario diario
+# corresponde a 400, tambien que ingrese cuantos dias falto a su trabajo y segun esto identificar
+# cual sera su salario en el mes.
+
+# salario=int(input("Ingrese 1 si su salario diario es de 200 o 2 si su salrio diario es de 400: "))
+# dias=int(input("Ingrese cuantos dias faltaste en el mes: "))
+# if salario==1:
+#     pagoMes=((30-dias)*200)
+#     print(pagoMes)
+# else:
+#     pagoMes=(30-dias)*400
+#     print("Su salario es de:",pagoMes)
+
+# 2. calcular el factorial de un numero
+
+# def factorial(n):
+#     fac = int(1)
+#     for i in range(1, n+1):
+#       fac =fac*i
+#     return fac
+# for i in range(1,2):
+#    num  = int(input("Por favor digite un numero: "))
+#    print("El resultado del factorial es: "+str(factorial(num)))
+
+
+    
+
+# 3. calcular el promedio de 3 notas de un estudiante y 
+# mostrar si el estudiante aprobo o reprobo  la asignatura
+
+# nota1=float(input("Ingrese la nota 1: "))
+# nota2=float(input("Ingrese la nota 2: "))
+# nota3=float(input("Ingrese la nota 3: "))
+# promedio=(nota1+nota2+nota3)/3
+# if promedio<3:
+#     print("Su promedio es de: ",promedio)
+#     print("Reprobado")
+# else:
+#     print("Su promedio es de: ",promedio)
+#     print("Aprobado")
+
+# 4.Realizar un algoritmo que atraves de un arreglo unidimensional
+# almacene el numero total de kg de arroz cosechado durante cada mes, 
+# proporcionando la siguiente informacion: 
+# -calcular el promedio anual de kg cosechados.
+# -en cuantos y en que meses se obtuvieron cosechas superiores
+# al promedio anual.
+# -que mes tuvo la mayor cosecha
+
+# enero=float(input("Ingrese los Kg cosechados: "))
+# febrero=float(input("Ingrese los Kg cosechados: "))
+# marzo=float(input("Ingrese los Kg cosechados: "))
+# abril=float(input("Ingrese los Kg cosechados: "))
+# mayo=float(input("Ingrese los Kg cosechados: "))
+# junio=float(input("Ingrese los Kg cosechados: "))
+# julio=float(input("Ingrese los Kg cosechados: "))
+# agosto=float(input("Ingrese los Kg cosechados: "))
+# septiembre=float(input("Ingrese los Kg cosechados: "))
+# octubre=float(input("Ingrese los Kg cosechados: "))
+# noviembre=float(input("Ingrese los Kg cosechados: "))
+# diciembre=float(input("Ingrese los Kg cosechados: "))
+# promedioAnual=(enero+febrero+marzo+abril+mayo+junio+julio+agosto+septiembre+octubre+noviembre+diciembre)/12
+# def mesesSuperiores(n):
+
+
+
